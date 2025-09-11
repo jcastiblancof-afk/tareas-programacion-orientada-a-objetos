c05a3c28984b4eee9fd93a710db4f9e83e6ad967
Camilo F. <jcastiblancof@unal.edu.co>
2025-09-11 14:35:50 -0500

    Add files via upload

--- perrito feliz.py
+++ perrito feliz.py
@@ -9,18 +9,18 @@ class Perro:
     
 def ingresar_datos():
     # Ingresar datos para el Perro
-    print("¿como hizo feliz a su perro?...")
-    comida_perro = input("alimento de hoy: ")
-    juego_perro = input("cual fue el juego de hoy:")
-    descanso_perro = int(input ("cuantas horas descanso el perro: "))
+    print("¿Cómo hizo feliz a su perro?")
+    comida_perro = input("Alimento de hoy: ")
+    juego_perro = input("¿Cuál fue el juego de hoy?: ")
+    descanso_perro = int(input("¿Cuántas horas descansó el perro?: "))
 
-    perro=perro(comida_perro, juego_perro, descanso_perro)
+    perro = Perro(comida_perro, juego_perro, descanso_perro)
     return perro 
+
 def main():
-    perro=ingresar_datos()
-    print("felicidad del perro:")
-    print(f" el perro comio {perro.comida}, jugaron con el al {perro.juego} y durmior {perro.descanso} horas. Por lo tanto el perro esta feliz ")
-    if __name__ == "__main__":
-        main()
+    perro = ingresar_datos()
+    print("Felicidad del perro:")
+    print(f"El perro comió {perro.comida}, jugaron con el/la {perro.juego} y durmió {perro.descanso} horas. Por lo tanto, el perro está feliz.")
 
-
+if __name__ == "__main__":
+    main()
